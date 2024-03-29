a6d5d63006e3466c32374eb32882736ab21c7b54
EC2 Default User <ec2-user@ip-172-31-56-23.ec2.internal>
2024-03-29 11:02:24 +0000

    lesson3 exercise

--- app.js
+++ app.js
@@ -1,40 +1,108 @@
-// alert('Hello World');
+// // 関数 呼び出し
 
-// // 変数
-// let str1 = 'Hello';
-// let str2 = 'World!!';
-// alert(str1 + str2);
+// // メイン部分
+// let alertString;
 
-// // if文
-// let orange = 100;
-// let apple = 120;
+// // 作成した関数を呼び出し、変数へ格納
+// alertString = addString("WebCamp");
 
-// if(orange < apple){
-//   alert('みかんの値段がりんごより安い');
-// } else if(orange == apple){
-//   alert('みかんとりんごが同じ値段');
-// } else{
-//   alert('みかんの値段がりんごより高い');
+// //変数の中身をアラートで表示する
+// alert(alertString);
+
+// // 作成した関数
+// function addString(strA){
+//   let addStr = "Hello " + strA;
+//   return addStr;
 // }
 
-// // while文
-// let max = 100;
-// let num = 1;
-// let count = 0;
 
-// while(num < max){
-//   num = num * 2;
-//   count = count + 1;
-// }
+// // 複数関数での表示
+// let promptStr = prompt('何か好きな文字を入力してください。');
 
-// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+// alert(promptStr);
 
-// for文
-let i;
-let num = 0;
 
-for(i = 1; i < 11; i++){
-  num = num + i;
+// じゃんけんプログラム
+
+// じゃんけんの手を入力してもらうプロンプト欄を生成
+let flag = false
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+while (flag == false) {
+  alert('グー・チョキ・パーのいずれかを入力してください')
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+  flag = FlagHand(user_hand)
 }
 
-alert('1から10まで足し算した結果は' + num + 'です');
+// じゃんけんの手をランダムに作成する関数を呼び出す
+let js_hand = getJShand();
+
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
+let judge = winLose(user_hand, js_hand);
+
+if (flag == null) {
+  alert('またチャレンジしてね')
+} else {
+  // 結果を表示する
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+}
+
+
+// ランダムでじゃんけんの手を作成する関数
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
+  let hand_name;
+
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  } else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  } else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+
+  return hand_name;
+}
+
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+function winLose(user, js){
+  let winLoseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
+
+function FlagHand(user_hand){
+  let flag = false
+  if (user_hand == "グー" || user_hand == "チョキ" || user_hand == "パー"){
+    flag = true
+  } else if(user_hand == null) {
+    flag = null
+  }
+  return flag
+}
